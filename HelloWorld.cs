c9a46642692ed93f77be2e1076dd08f9e6dfe2d6
RichardLi1437 <richard_li_1437@hotmail.com>
2016-11-01 16:58:53 -0700

    add 3rd options

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,5 +4,6 @@ public class HelloWorld {
 	static void main(string[] args) {
 		Console.WriteLine("Hello World!");
 		Console.WriteLine("{0} {1}!", "Hello", "World");
+		Console.WriteLine(string.Format("{0} {1}!", "Hello", "WOrld"));
 	}
 }
