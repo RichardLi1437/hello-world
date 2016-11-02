cd6a299afe6f7e8f48a816f4c00f761f45fe4e8e
RichardLi1437 <richard_li_1437@hotmail.com>
2016-11-01 17:09:19 -0700

    fix a typo

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,6 +4,6 @@ public class HelloWorld {
 	static void main(string[] args) {
 		Console.WriteLine("Hello World!");
 		Console.WriteLine("{0} {1}!", "Hello", "World");
-		Console.WriteLine(string.Format("{0} {1}!", "Hello", "WOrld"));
+		Console.WriteLine(string.Format("{0} {1}!", "Hello", "World"));
 	}
 }
